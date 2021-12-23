e9d8a569175ae363528986de9b1f969d84c199b6
Alex Alan Nunes <alexalannunes@gmail.com>
2021-12-23 11:56:49 -0300

    remove console

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
-console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
